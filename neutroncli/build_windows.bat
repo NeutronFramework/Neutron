c8c1ac074d3871a389c368f671b419cb8b2f75c2
Annas <annaskherunas06@gmail.com>
2024-11-12 13:55:30 +0700

    trying to pass chocolatey verification

--- neutroncli/build_windows.bat
+++ neutroncli/build_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set version=0.2.3
+set version=0.2.4
 set PROJECT_URL=https://github.com/NeutronFramework/Neutron
 
 where choco >nul 2>&1
--- neutroncli/publish_to_chocolatey.bat
+++ neutroncli/publish_to_chocolatey.bat
@@ -1,3 +1,3 @@
-set version=0.2.3
+set version=0.2.4
 
 choco push build\artifacts\neutroncli.%version%.nupkg --source https://push.chocolatey.org/ --api-key %CHOCOLATEY_API_KEY%
